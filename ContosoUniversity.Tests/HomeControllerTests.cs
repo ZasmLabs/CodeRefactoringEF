113fbf5b8442e1ba6c8b61c687ce30fa5a872bad
Ziaullah Khan <khan.ziaullah@gmail.com>
2020-01-12 20:02:51 +0530

    upgrade to .net 4.8. fixed broken navigation bar

---
+++ ContosoUniversity.Tests/HomeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ContosoUniversity.Controllers;
+using ContosoUniversity.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContosoUniversity.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void AboutPageRuturnsFiveRowsOfEnrollments()
+        {
+            // Arrange
+            var homeController = new HomeController();
+            var expectedCount = 5;
+            int actualCount = 0;
+
+            // Act
+            var actionResult = homeController.About() as ViewResult;
+            var model = actionResult.ViewData.Model as IEnumerable<EnrollmentDateGroup>;
+            actualCount = model.Count();
+
+            // Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
